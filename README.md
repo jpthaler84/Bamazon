09aefb3ea362db1b2810ef8f39b02d727770fc90
Jordan Thaler <jordanpthaler@gmail.com>
2017-11-24 10:40:44 -0600

    updated readme, added gitignore, cleaned code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # bamazon
+
+Need something exactly like Amazon? Try Bamazon! It's a node.js-based app that uses a MySQL database to keep track of inventory prices and stock.
+
+Future Developments: 
+
+	*Implement Supervisor and Manager menus with options to manage various aspects of the app/database
+	*Develop a GUI
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,59 +3,87 @@ var inquirer = require("inquirer");
 
 // Create connection
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
+    host: "localhost",
+    port: 3306,
 
-  // Username
-  user: "root",
+    // Username
+    user: "root",
 
-  // Password
+    // Password
     // Look into how to hide this...
-  password: "Ocelot84!",
-  database: "bamazon"
+    password: "",
+    database: "bamazon"
 });
 
 // Connect to MySQL server and database
 connection.connect(function(err) {
-  if (err) throw err;
-  // Run start function
-  inventory();
+    if (err) throw err;
+    // Run start function
+    inventory();
 });
 
 // Displays entire inventory (IDs, Names, & Prices)
 function inventory() {
-  console.log("**************************");
-  console.log("***Welcome to Bamazon!!***");
-  console.log("**************************");
-  console.log("");
-  console.log("");
-  connection.query("SELECT item_id, product_name, price FROM products", function(err, res) {
-    if (err) throw err;
-    console.log(res);
-    connection.end();
-  });
+    console.log("**************************");
+    console.log("***Welcome to Bamazon!!***");
+    console.log("**************************");
+    console.log("");
+    console.log("");
+    connection.query("SELECT item_id, product_name, price FROM products", function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+        start();
+    });
 }
 
 // Function to display entire inventory
+function start(inventory) {
+    inquirer
+        .prompt({
+            name: "itemId",
+            type: "input",
+            message: "What is the product ID of the item you would like to purchase?",
+        })
+        .then(function(answer) {
+            var itemId = parseInt(val.itemId);
+
+            if (inventoryCheck(itemId,inventory)) {
+              itemQuantity(product);
+            }
+            });
+      };
+
+// Check inventory
+function inventoryCheck(itemId, inventory) {
+  for (var i = 0; i < inventory.length; i++) {
+    if (inventory[i].item_id === choiceId) {
+
+      return inventory[i];
+    }
+}
+};
 
 
-function start() {
+// Prompt the customer for a product quantity
+function itemQuantity(product) {
   inquirer
-    .prompt({
-      name: "itemId",
-      type: "input",
-      message: "What is the product ID of the item you would like to purchase?",
-    })
-    .then(function(answer) {
-      // based on their answer, either call the bid or the post functions
-      if (answer.postOrBid === "POST") {
-        postAuction();
+    .prompt([
+      {
+        type: "input",
+        name: "quantity",
+        message: "How many would you like to purchase?",
       }
-      else if (answer.postOrBid === "BID") {
-        bidAuction();
+    ])
+    .then(function(val) {
+      var quantity = parseInt(val.quantity);
+
+      if (quantity > product.stock_quantity) {
+        console.log("Insufficient quantity.");
+        loadProducts();
       }
       else {
-        connection.end();
+        makePurchase(product, quantity);
       }
     });
-}
+  };
